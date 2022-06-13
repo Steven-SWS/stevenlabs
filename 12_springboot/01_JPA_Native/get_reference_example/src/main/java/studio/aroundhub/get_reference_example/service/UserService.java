23f5d7146771581836f4ba0cfbb19d3f96db7192
Steven-SWS <directerking@naver.com>
2022-06-13 17:58:37 +0900

    JPA Simple Code 업로드

---
+++ 12_springboot/01_JPA_Native/get_reference_example/src/main/java/studio/aroundhub/get_reference_example/service/UserService.java
@@ -0,0 +1,15 @@
+package studio.aroundhub.get_reference_example.service;
+
+import java.util.List;
+import java.util.Optional;
+import studio.aroundhub.get_reference_example.entity.UserEntity;
+
+public interface UserService {
+
+    void saveUser(UserEntity userEntity);
+
+    Optional<UserEntity> getUser(String email);
+
+    Optional<UserEntity> getReferenceUser(String email);
+
+}
---
+++ 12_springboot/01_JPA_Native/simple_example/src/main/java/studio/aroundhub/simple_example/entity/UserEntity.java
@@ -0,0 +1,63 @@
+package studio.aroundhub.simple_example.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    // Entity 에서는 final 키워드를 사용하면 안됨
+    // 일부 기능에서 예외가 발생하여 정상적으로 동작하지 않을 수 있음
+    @Id
+    private String email;
+
+    private String name;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public UserEntity(String email, String name, LocalDateTime createdAt,
+                      LocalDateTime updatedAt) {
+        this.email = email;
+        this.name = name;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    // JPA 규약으로 기본 생성자를 제공해야 함
+    public UserEntity(){
+
+    }
+
+    public void changeName(String newName){
+        this.name = newName;
+        updateDateTime();
+    }
+
+    private void updateDateTime(){
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
